f3e61ef5b33d95ac0a80710b7ffa14076d68a901
JP Osorio <jposorio@kenzan.com>
2015-10-12 13:17:18 -0400

    KEY-9: Added test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,21 +1,25 @@
 /*global require */
-// 'use strict';
-//
-// var validatePipeline = require('../');
-// var gulp = require('gulp');
-// var path = require('path');
-// var assert = require('stream-assert');
-// var should = require('chai').should();
-// var isStream = require('isstream');
-//
-// var fixtures = function (glob) { return path.join(__dirname, 'fixtures', glob); };
-//
-// describe('pipeline-validate-css', function() {
-//
-//   var testPath = path.join(__dirname, 'dist');
-//
-//   it('Should return a stream after validation', function (done) {
-//
-//   });
-//
-// });
+'use strict';
+
+var validatePipeline = require('../');
+var gulp = require('gulp');
+var path = require('path');
+var should = require('chai').should();
+var isStream = require('isstream');
+
+var fixtures = function (glob) { return path.join(__dirname, 'fixtures', glob); };
+
+describe('pipeline-validate-css', function() {
+
+  var testPath = path.join(__dirname, 'dist');
+
+  it('Should return the stream after validation', function (done) {
+    var stream = gulp
+      .src(fixtures('*'))
+      .pipe(validatePipeline().validateCSS());
+
+      isStream(stream).should.equal(true);
+      done();
+  });
+
+});
